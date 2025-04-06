afc8964a72219ae328282f4c0b5a271cd4aec9c8
aleks <aleksandrroznov484@gmail.com>
2025-04-06 19:24:25 +0300

    add methods for get info from db by day/year/month

--- time-tracking-service/src/main/java/com/example/timetrackingservice/repository/WorkLogRepo.java
+++ time-tracking-service/src/main/java/com/example/timetrackingservice/repository/WorkLogRepo.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,5 +15,14 @@ public interface WorkLogRepo extends JpaRepository<WorkLog, Long> {
     @Query("SELECT w FROM WorkLog w WHERE w.userId = :userId ORDER BY w.startTime DESC LIMIT 1")
     Optional<WorkLog> findLastByUserId(@Param("userId") Long userId);
     List<WorkLog> findAllByUserId(Long userId);
-    //todo методы для get по любому из Отрезков времени
+
+    @Query("select w from WorkLog w where DATE(w.startTime) = :day")
+    List<WorkLog> findAllByDate(@Param("day") LocalDate day);
+
+    @Query("select w from WorkLog w where year(w.startTime) = :year")
+    List<WorkLog> findAllByYear(@Param("year") Integer year);
+
+    @Query("select w from WorkLog w where MONTH(w.startTime) = :month and  YEAR(w.startTime) = :year")
+    List<WorkLog> findAllByMonthAndYear(@Param("month") Integer month, @Param("year") Integer year);
+
 }
