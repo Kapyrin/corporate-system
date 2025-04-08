4240494a0faee104af6d2473d0b98a8e1eb60824
aleks <aleksandrroznov484@gmail.com>
2025-04-08 18:36:38 +0300

    add controller post methods

--- time-tracking-service/src/main/java/com/example/timetrackingservice/controller/TimeTrackRestController.java
+++ time-tracking-service/src/main/java/com/example/timetrackingservice/controller/TimeTrackRestController.java
@@ -1,5 +1,6 @@
 package com.example.timetrackingservice.controller;
 
+import com.example.timetrackingservice.dto.WorkLogReportDto;
 import com.example.timetrackingservice.dto.WorkLogResponseDto;
 import com.example.timetrackingservice.service.WorkLogService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -7,6 +8,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/time")
@@ -32,4 +35,25 @@ public class TimeTrackRestController {
         return ResponseEntity.status(201).body(responseDto);
     }
 
+    @Operation(summary = "Get report by user id and data period")
+    @ApiResponse(responseCode = "200", description = "Report found")
+    @ApiResponse(responseCode = "404", description = "Report not found")
+    @GetMapping("/{userId}")
+    public ResponseEntity<WorkLogReportDto> getReport(@PathVariable("userId") Long id, @RequestParam String date) {
+        return ResponseEntity.ok(workLogService.getReport(id, date));
+    }
+    @Operation(summary = "Get report by report id")
+    @ApiResponse(responseCode = "200", description = "Report found")
+    @ApiResponse(responseCode = "404", description = "Report not found")
+    @GetMapping("/report/{id}")
+    public ResponseEntity<WorkLogResponseDto> getReportById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(workLogService.getWorkLogById(id));
+    }
+    @Operation(summary = "Get reports by report user id")
+    @ApiResponse(responseCode = "200", description = "Reports by user was found")
+    @ApiResponse(responseCode = "404", description = "Reports by user was not found")
+    @GetMapping("/reports/{userId}")
+    public ResponseEntity<List<WorkLogResponseDto>> getReportsByUserId(@PathVariable("userId") Long userId) {
+        return ResponseEntity.ok(workLogService.getAllLogsByUserId(userId));
+    }
 }
