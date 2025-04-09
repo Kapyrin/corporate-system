e2aa7a153c26bb1ae173fe4e16ef17fed99044d7
aleks <aleksandrroznov484@gmail.com>
2025-04-09 19:47:25 +0300

    add delete and get by range endpoints

--- time-tracking-service/src/main/java/com/example/timetrackingservice/controller/TimeTrackRestController.java
+++ time-tracking-service/src/main/java/com/example/timetrackingservice/controller/TimeTrackRestController.java
@@ -4,11 +4,13 @@ import com.example.timetrackingservice.dto.WorkLogReportDto;
 import com.example.timetrackingservice.dto.WorkLogResponseDto;
 import com.example.timetrackingservice.service.WorkLogService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -42,6 +44,7 @@ public class TimeTrackRestController {
     public ResponseEntity<WorkLogReportDto> getReport(@PathVariable("userId") Long id, @RequestParam String date) {
         return ResponseEntity.ok(workLogService.getReport(id, date));
     }
+
     @Operation(summary = "Get report by report id")
     @ApiResponse(responseCode = "200", description = "Report found")
     @ApiResponse(responseCode = "404", description = "Report not found")
@@ -49,6 +52,7 @@ public class TimeTrackRestController {
     public ResponseEntity<WorkLogResponseDto> getReportById(@PathVariable("id") Long id) {
         return ResponseEntity.ok(workLogService.getWorkLogById(id));
     }
+
     @Operation(summary = "Get reports by report user id")
     @ApiResponse(responseCode = "200", description = "Reports by user was found")
     @ApiResponse(responseCode = "404", description = "Reports by user was not found")
@@ -56,4 +60,26 @@ public class TimeTrackRestController {
     public ResponseEntity<List<WorkLogResponseDto>> getReportsByUserId(@PathVariable("userId") Long userId) {
         return ResponseEntity.ok(workLogService.getAllLogsByUserId(userId));
     }
+
+    @Operation(summary = "Get reports by date range")
+    @ApiResponse(responseCode = "200", description = "Reports by date range was found")
+    @ApiResponse(responseCode = "404", description = "Reports by date range was not found")
+    @GetMapping("/reports")
+    public ResponseEntity<List<WorkLogResponseDto>> getReportsDataRange(
+            @Parameter(description = "Start date/time in format YYYY-MM-DDThh:mm:ss", example = "2023-01-01T00:00:00")@RequestParam LocalDateTime from,
+            @Parameter(description = "End date/time in format YYYY-MM-DDThh:mm:ss", example = "2023-12-31T23:59:59") @RequestParam LocalDateTime to)
+    {
+        return ResponseEntity.ok(workLogService.getWorkLogsByDateRange(from,to));
+    }
+
+
+    @Operation(summary = "Delete report by report id")
+    @ApiResponse(responseCode = "200", description = "Report deleted")
+    @ApiResponse(responseCode = "404", description = "Report not deleted")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteReportById(@PathVariable("id") Long id) {
+        workLogService.deleteWorkLogById(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
