9fe99f0dc6a2fc79dcb9bd93616626c77cae3843
Vladimir Kapyrin <garage@yanex.ru>
2025-05-09 12:19:21 +0100

    Prepared for telegram bot.

--- telegram-bot/src/main/java/ru/kapyrin/telegrambot/repository/TelegramUserRepository.java
+++ telegram-bot/src/main/java/ru/kapyrin/telegrambot/repository/TelegramUserRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.kapyrin.telegrambot.entity.TelegramUser;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface TelegramUserRepository extends JpaRepository<TelegramUser, Long> {
     boolean existsByUserIdAndTelegramId(Long userId, Long telegramId);
--- telegram-bot/src/main/java/ru/kapyrin/telegrambot/service/TelegramNotificationBot.java
+++ telegram-bot/src/main/java/ru/kapyrin/telegrambot/service/TelegramNotificationBot.java
@@ -3,22 +3,25 @@ package ru.kapyrin.telegrambot.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import ru.kapyrin.telegrambot.exception.NotificationException;
-import ru.kapyrin.telegrambot.exception.NotificationSendException;
 
 @Component
 @Slf4j
 public class TelegramNotificationBot extends TelegramLongPollingBot {
 
-    @Value("${telegram.bot.token}")
-    private String token;
 
-    @Value("${telegram.bot.username}")
-    private String username;
+    private final String username;
 
+    public TelegramNotificationBot(
+            @Value("${telegram.bot.token}") String token,
+            @Value("${telegram.bot.username}") String username
+    ) {
+        super(token);
+        this.username = username;
+    }
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
@@ -28,7 +31,7 @@ public class TelegramNotificationBot extends TelegramLongPollingBot {
             if (msg.equals("/start")) {
                 log.info("New /start received from telegramId (chatId): {}", chatId);
                 sendText(chatId, """
-                            Welcome! 
+                        Welcome! 
                         Your Telegram ID is: %d
                         
                         Please share this ID with your system admin or link it manually using:
@@ -45,11 +48,6 @@ public class TelegramNotificationBot extends TelegramLongPollingBot {
     @Override
     public String getBotUsername() {
         return username;
-    }
-
-    @Override
-    public String getBotToken() {
-        return token;
     }
 
     public void sendText(Long chatId, String text) {
