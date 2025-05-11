48fcb06b290baa54cc97b31ffe98ae3350de2f5e
aleks <aleksandrroznov484@gmail.com>
2025-05-11 20:06:54 +0300

    Start adding Rabbit

---
+++ notification-service/src/main/java/ru/roznov/rabbitservice/config/MessageSender.java
@@ -0,0 +1,24 @@
+package ru.roznov.rabbitservice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MessageSender {
+    @Value("${queue.name.telegram}")
+    private String telegramQueueName;
+
+    @Value("${queue.name.notify}")
+    private String notificationQueueName;
+    private final RabbitTemplate rabbitTemplate;
+
+    public void sendToTelegram(String message) {
+        rabbitTemplate.convertAndSend(telegramQueueName,message);
+    }
+    public void sendToNotification(String message) {
+        rabbitTemplate.convertAndSend(notificationQueueName, message);
+    }
+}
